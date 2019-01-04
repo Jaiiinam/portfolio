0fbb85a7aa9639549cf0d8953e58972e25d74e43
Jaiiinam <46359629+Jaiiinam@users.noreply.github.com>
2019-01-03 19:00:25 -0500

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php 
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $formcontent="From: $name \n Message: $message";
+    $recipient = "jainampatel93@gmail.com";
+    $subject = "Job";
+    $mailheader = "From: $email \r\n";
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+    echo "Thank You!";
+?>
